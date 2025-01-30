76c8a91982df4b60be8fb5b03cd0bf571580190f
Pasan Hettiarachchi <pasan.hettiarachchi@medsci.uu.se>
2025-01-30 16:34:49 +0100

    added license

--- ActiPASS_example_workflow.m
+++ ActiPASS_example_workflow.m
@@ -1,4 +1,37 @@
-%% intialize variables and load Settings
+%% ActiPASS Example workflow for understanding how it works
+% WARNING: 
+% This is a very basic basic example workflow. Actual ActiPASS workflow used in the GUI is much more robust and
+% contains improved error handling including, visualizations, quality-checks and fallbacks to alternative methods
+% for reference-position corrections.
+% Use this just as an example script how ActiPASS algorithms can be used. 
+
+% Copyright (c) 2024, Pasan Hettiarachchi
+% All rights reserved.
+% 
+% Redistribution and use in source and binary forms, with or without 
+% modification, are permitted provided that the following conditions are met: 
+% 1. Redistributions of source code must retain the above copyright notice, 
+%    this list of conditions and the following disclaimer.
+% 2. Redistributions in binary form must reproduce the above copyright notice, 
+%    this list of conditions and the following disclaimer in the documentation 
+%    and/or other materials provided with the distribution.
+% 3. Neither the name of the copyright holder nor the names of its contributors
+%    may be used to endorse or promote products derived from this software without
+%    specific prior written permission.
+% 
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+% POSSIBILITY OF SUCH DAMAGE. 
+
+%% Initialize 
 clear variables;
 acc_file="C:\Users\xxxxx\Downloads\testdata.csv";
 out_file="C:\Users\xxxxx\Downloads\ActiPASS_output.csv";
@@ -178,7 +211,8 @@ if ~strcmpi(statusBdTime,"OK")
     warning("Error calculating times-of-bed and sleep");
 end
 fprintf('%s Now exporting output to a CSV file....\n',string(datetime,"HH:mm:ss.SSS"));
-% write detected activity time-series to a file
+
+%% write detected activity time-series to a file
 writetable(table(timeFull',actFull','VariableNames',["Time","Activity"]),out_file);
 % show elapsed time
 toc;
